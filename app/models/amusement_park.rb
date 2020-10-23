a36abdd2e3399526771607b84edf2b159e0dc9b3
Joshua Carey <jdcarey128@gmail.com>
2020-10-23 10:32:23 -0600

    Add price display for amusement park

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -2,4 +2,8 @@ class AmusementPark < ApplicationRecord
   has_many :rides
   validates_presence_of :name, :admission_price
 
+  def price_display
+    "$" + self.admission_price.to_s + ".00"
+  end
+
 end
--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe AmusementPark, type: :model do
   describe 'relationships' do
     it { should have_many :rides }
   end
+
+  describe 'instance_methods' do
+    it 'price_display' do
+      amusement_park = AmusementPark.create!(name: "Cedar Point", admission_price: 100)
+
+      expect(amusement_park.price_display).to eq("$100.00")
+    end
+  end
 end
